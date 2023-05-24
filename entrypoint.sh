5fc3cd91c91a67e8021808bf54b8635b34e1513b
madebytimo <t.schnaible@madebytimo.de>
2023-05-24 13:11:29 +0200

    add log output

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,7 +6,8 @@ mkdir --parents /media/downloader/fail
 mkdir --parents /media/downloader/output
 mkdir --parents /media/workdir
 
-echo "$DOWNLOADER_CRON root DOWNLOADER_PLAYLIST_URLS=\"${DOWNLOADER_PLAYLIST_URLS}\" /app/downloader.sh" \
-    > /media/cron/downloader
+echo "$DOWNLOADER_CRON root DOWNLOADER_PLAYLIST_URLS=\"${DOWNLOADER_PLAYLIST_URLS}\"" \
+    "bash --login -c '/app/downloader.sh > /proc/1/fd/1 2>&1'" > \
+    /media/cron/downloader
 
 exec /entrypoint-cron.sh "$@"
